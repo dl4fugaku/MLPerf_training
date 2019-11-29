0e1c9efb19ccb7ac92a3907d328e3768bffa18e9
Jens Domke <28772296+jdomke@users.noreply.github.com>
2019-11-29 18:26:16 +0900

    mod run script for getting scorep data on xeon

--- image_classification/tensorflow/run.sh
+++ image_classification/tensorflow/run.sh
@@ -6,17 +6,17 @@ set -e
 
 # Register the model as a source root
 export PYTHONPATH="$(pwd):$(pwd)/../../compliance:${PYTHONPATH}"
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64
 
 MODEL_DIR="/tmp/resnet_imagenet_${RANDOM_SEED}"
 rm -rf $MODEL_DIR
 
+#SCOREP_TOTAL_MEMORY=4089446400 SCOREP_ENABLE_PROFILING=true SCOREP_ENABLE_TRACING=false \
+SCOREP_TOTAL_MEMORY=4089446400 SCOREP_ENABLE_PROFILING=false SCOREP_ENABLE_TRACING=true \
 OMP_NUM_THREADS=16 TF_NUM_INTEROP_THREADS=2 TF_NUM_INTRAOP_THREADS=16 \
-  python3.6 official/resnet/imagenet_main.py $RANDOM_SEED --data_dir /scr0/jens/data/imagenet/train  \
-  --model_dir $MODEL_DIR --train_epochs 1 --stop_threshold $QUALITY --batch_size 128 \
+  python3.7 -m scorep --nopython official/resnet/imagenet_main.py $RANDOM_SEED --data_dir /home/domke/data/imagenet/train  \
+  --model_dir $MODEL_DIR --train_epochs 1 --stop_threshold $QUALITY --batch_size 64 \
   --version 1 --resnet_size 50 --epochs_between_evals 1 \
-  --max_train_steps 1000 --num_gpus 1 --data_format channels_first \
-  --inter_op_parallelism_threads 2 --intra_op_parallelism_threads 16
+  --max_train_steps 1000 --inter_op_parallelism_threads 2 --intra_op_parallelism_threads 16
 #  --model_dir $MODEL_DIR --train_epochs 10000 --stop_threshold $QUALITY --batch_size 64 \
 #  --version 1 --resnet_size 50 --epochs_between_evals 4
 
